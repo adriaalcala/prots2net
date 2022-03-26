9cb6ca19bfa7b7c722c2f4fac43bee0770fcea89
Adria Alcala <adria.alcala@gmail.com>
2022-03-26 10:12:41 +0100

    remove extra files

--- extra_files/build_fasta.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open('../data/others/salinibacter_all.csv') as f:
-    reader = csv.reader(f)
-    rows = list(reader)
-
-with open('../data/secuences/sal.faa', 'w') as f:
-    for row in rows[1:]:
-        f.write(f">{row[0]}\n")
-        f.write(f"{row[1]}\n")
