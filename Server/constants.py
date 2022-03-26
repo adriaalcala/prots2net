8703d1473aecd88a148a110f33bd6a09f8565ec0
Adria Alcala <adria.alcala@gmail.com>
2022-03-26 11:07:43 +0100

    clean server file

---
+++ Server/constants.py
@@ -0,0 +1,5 @@
+STRING_VERSION = "11.5"
+STRING_URL = "https://stringdb-static.org/download/"
+INTERACTION_THRESHOLD = 999
+STRING_API = "https://string-db.org/api/json"
+DEFAULT_BLAST_METHOD = 'STRING'
--- Server/main.py
+++ Server/main.py
@@ -5,94 +5,82 @@ import random
 import requests
 import subprocess
 
+from joblib import dump, load
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from Bio import SeqIO
 
+from logger import logger
+from Server.constants import DEFAULT_BLAST_METHOD, INTERACTION_THRESHOLD, STRING_API, STRING_URL, STRING_VERSION
 
-def create_secuences_file(input_file, species1):
-    print(input_file, species1)
+
+def prepare_paths():
+    path_used = ['blast', 'edges', 'graphs', 'nets', 'sequences', 'models']
+    for path in path_used:
+        if not os.path.exists(f"data/{path}"):
+            logger.debug(f"Creating path data/{path}")
+            os.makedirs(f"data/{path}")
+
+
+def create_sequences_file(input_file, species1):
     if input_file is None:
-        get_secuences(species1)
+        get_sequences(species1)
     else:
-        bashcommand = f'cp {input_file} data/secuences/{species1}.faa'
-        subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-
-def read_info(input_file):
-    pass
+        bash_command = f'cp {input_file} data/sequences/{species1}.faa'
+        subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
 
 
-def get_secuences(species: str, use_cache=True):
-    if os.path.isfile(f'data/secuences/{species}.protein.sequences.v11.5.fa') and use_cache:
+def get_file(url: str, output: str, destiny: str, compressed=True):
+    output_curl = output if not compressed else f"{output}.gz"
+    bash_command = f"curl {url} --output {output_curl}"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+    if compressed:
+        bash_command = f"gunzip {output_curl}"
+        process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+        output, _ = process.communicate()
+    bash_command = f"mv {output} {destiny}"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, _ = process.communicate()
+
+
+def get_sequences(species: str, use_cache=True):
+    if os.path.isfile(f'data/sequences/{species}.protein.sequences.{STRING_VERSION}.fa') and use_cache:
         return None
-    url = f"https://stringdb-static.org/download/protein.sequences.v11.5/{species}.protein.sequences.v11.5.fa.gz"
-    bashcommand = f"curl {url} --output {species}.fa.gz"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
-    bashcommand = f"gunzip {species}.fa.gz"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
-    bashcommand = f"mv {species}.fa data/secuences/{species}.protein.sequences.v11.5.fa"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
+    url = f"{STRING_URL}/protein.sequences.{STRING_VERSION}/{species}.protein.sequences.{STRING_VERSION}.fa.gz"
+    get_file(url, "{species}.fa", f"data/sequences/{species}.protein.sequences.{STRING_VERSION}.fa")
 
-
-def download_edges(species: str):
-    url = f"https://stringdb-static.org/download/protein.links.v11.5/{species}.protein.links.v11.5.txt.gz"
-    bashcommand = f"curl {url} --output {species}_links.txt.gz"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
-    bashcommand = f"gunzip {species}_links.txt.gz"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
-    bashcommand = f"mv {species}_links.txt data/edges/{species}.protein.links.v11.5.txt"
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    print(output)
-    
 
 def get_edges(species: str, use_cache=True):
-    if not os.path.isfile(f'data/edges/{species}.protein.links.v11.5.txt') and use_cache:
-        download_edges(species)
-    with open(f'data/edges/{species}.protein.links.v11.5.txt') as f:
+    if not os.path.isfile(f'data/edges/{species}.protein.links.{STRING_VERSION}.txt') and use_cache:
+        url = f"{STRING_URL}/protein.links.{STRING_VERSION}/{species}.protein.links.{STRING_VERSION}.txt.gz"
+        get_file(url, f"{species}_links.txt", f"data/edges/{species}.protein.links.{STRING_VERSION}.txt")
+    with open(f'data/edges/{species}.protein.links.{STRING_VERSION}.txt') as f:
         reader = csv.reader(f, delimiter=' ')
         rows = list(reader)
-    return {f"{e[0]}_{e[1]}": float(e[2]) for e in  rows[1:]}
+    return {f"{e[0]}_{e[1]}": float(e[2]) for e in rows[1:]}
 
 
 def build_dataset(species1, species2):
     blast_matrix = compute_blast(species1, species2)
-    prot_bitscore = {r['protein_1']: float(r['bit_score']) for r in blast_matrix}
+    prot_bit_score = {r['protein_1']: float(r['bit_score']) for r in blast_matrix}
     prot_prot = {r['protein_1']: r['protein_2'] for r in blast_matrix}
     edges = get_edges(species1)
     edges_2 = get_edges(species2)
-    prots = list(prot_bitscore.keys())
-    print('start build dataset')
+    prots = list(prot_bit_score.keys())
     training_data = []
     result = []
     train_prots = []
-    i = 0
-    total = len(prots)
     for p11 in prots:
-        i += 1
-        if random.random() > 0.8:
-            print(i, total)
         p21 = prot_prot[p11]
         for p12 in prots:
             p22 = prot_prot[p12]
             if random.random() > 0.9:
                 edge_2_value = edges_2.get(f"{p21}_{p22}") or edges_2.get(f"{p22}_{p21}", 0)
-                training_data.append([prot_bitscore[p11], prot_bitscore[p12], edge_2_value])
+                training_data.append([prot_bit_score[p11], prot_bit_score[p12], edge_2_value])
                 edge_1_value = edges.get(f"{p11}_{p12}") or edges.get(f"{p12}_{p11}", 0)
-                result.append(1 if edge_1_value > 700 else 0)
+                result.append(1 if edge_1_value > INTERACTION_THRESHOLD else 0)
                 train_prots.append([p11, p12])
-
-    print('end build dataset')
 
     return training_data, result
 
@@ -103,31 +91,38 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def compute_blast_new(species1, species2, write_result=True):
-    print('blast', species1, species2)
+def compute_blast(species1, species2, write_result=True, method=DEFAULT_BLAST_METHOD):
+    method_dispatcher = {
+        'STRING': compute_blast_string,
+        'LOCAL': compute_blast_local,
+        'SPLIT': compute_blast_split
+    }
+    if method not in method_dispatcher:
+        logger.error(f"Method {method} not available, using default method {DEFAULT_BLAST_METHOD}")
+        method = DEFAULT_BLAST_METHOD
+
+    return method_dispatcher[method](species1, species2, write_result=write_result)
+
+
+def compute_blast_string(species1, species2, write_result=True):
     if os.path.isfile(f'data/blast/result_{species1}_{species2}.csv'):
         with open(f'data/blast/result_{species1}_{species2}.csv') as f:
             reader = csv.DictReader(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
             return list(reader)
-    print('start compute blast')
-    fasta_sequences = SeqIO.parse(open(f"data/secuences/{species1}.protein.sequences.v11.5.fa"),'fasta')
+    fasta_sequences = SeqIO.parse(open(f"data/sequences/{species1}.protein.sequences.{STRING_VERSION}.fa"), 'fasta')
     names = [f.id for f in fasta_sequences]
     triads = []
-    i = 0
-    total = len(names) / 100
     species1 = species1.split('_')[0]
     for chunk in chunks(names, 100):
         identifiers = '%0d'.join(chunk)
-        url = f'https://string-db.org/api/json/homology_best?identifiers={identifiers}&species={species1}&species_b={species2}'
-        #print('url', url)
+        url = f'{STRING_API}/homology_best?identifiers={identifiers}&species={species1}&species_b={species2}'
         response = requests.get(url)
-        #print(response)
         result = response.json()
-        #print(result)
-        triads.extend([{'protein_1': r['stringId_A'], 'protein_2': r['stringId_B'], 'bit_score': r['bitscore']} for r in result])
-        i += 1
-        print(i, total)
-    print(triads[-1])
+        triads.extend([
+            {'protein_1': r['stringId_A'], 'protein_2': r['stringId_B'], 'bit_score': r['bitscore']}
+            for r in result
+        ])
+
     if write_result:
         with open(f'data/blast/result_{species1}_{species2}.csv', 'w') as f:
             writer = csv.DictWriter(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
@@ -135,47 +130,48 @@ def compute_blast_new(species1, species2, write_result=True):
     return triads
 
 
-def compute_blast(species1, species2, write_result=True):
-    if os.path.isfile(f'data/blast/result_{species1}_{species2}.csv'):
-        with open(f'data/blast/result_{species1}_{species2}.csv') as f:
-            reader = csv.DictReader(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
-            return list(reader)
-    print('start compute blast')
-    bashcommand = f'psiblast -query data/secuences/{species1}.protein.sequences.v11.5.fa -subject data/secuences/{species2}.protein.sequences.v11.5.fa -outfmt 15'
-    process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
+def compute_blast_aux(query: str, subject: str):
+    bash_command = f'psiblast -query {query} -subject {subject} -outfmt 15'
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, _ = process.communicate()
-    print('end compute blast')
     output_dict = json.loads(output)
-    triads = [get_most_similar_sequence(b['report']['results']['bl2seq'][0]) for b in output_dict['BlastOutput2']]
+    return [get_most_similar_sequence(b['report']['results']['bl2seq'][0]) for b in output_dict['BlastOutput2']]
+
+
+def compute_blast_local(species1, species2, write_result=True):
+    if os.path.isfile(f'data/blast/result_{species1}_{species2}.csv'):
+        with open(f'data/blast/result_{species1}_{species2}.csv') as f:
+            reader = csv.DictReader(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
+            return list(reader)
+    sequences1 = f"data/sequences/{species1}.protein.sequences.{STRING_VERSION}.fa"
+    sequences2 = f"data/sequences/{species2}.protein.sequences.{STRING_VERSION}.fa"
+    triads = compute_blast_aux(sequences1, sequences2)
+
     if write_result:
         with open(f'data/blast/result_{species1}_{species2}.csv', 'w') as f:
             writer = csv.DictWriter(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
             writer.writerows(triads)
     return triads
 
-def compute_blast_splitted(species1, species2, write_result=True):
+
+def compute_blast_split(species1, species2, write_result=True):
     if os.path.isfile(f'data/blast/result_{species1}_{species2}.csv'):
         with open(f'data/blast/result_{species1}_{species2}.csv') as f:
             reader = csv.DictReader(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
             return list(reader)
-    print('start compute blast')
     all_triads = []
-    for secuence_file in os.listdir(f'data/secuences/{species1}'):
-        print(secuence_file)
-        if os.path.isfile(f'data/blast/{species1}/result_{secuence_file.split(".")[0]}_{species2}.csv'):
-            with open(f'data/blast/{species1}/result_{secuence_file.split(".")[0]}_{species2}.csv') as f:
+    sequences2 = f"data/sequences/{species2}.faa"
+    for sequence_file in os.listdir(f'data/sequences/{species1}'):
+        if os.path.isfile(f'data/blast/{species1}/result_{sequence_file.split(".")[0]}_{species2}.csv'):
+            with open(f'data/blast/{species1}/result_{sequence_file.split(".")[0]}_{species2}.csv') as f:
                 reader = csv.DictReader(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
                 all_triads.extend(list(reader))
             continue
-        bashcommand = f'psiblast -query data/secuences/{species1}/{secuence_file} -subject data/secuences/{species2}.faa -outfmt 15'
-        process = subprocess.Popen(bashcommand.split(), stdout=subprocess.PIPE)
-        output, _ = process.communicate()
-        print('end compute blast')
-        output_dict = json.loads(output)
-        triads = [get_most_similar_sequence(b['report']['results']['bl2seq'][0]) for b in output_dict['BlastOutput2']]
+        sequences1 = f"data/sequences/{species1}/{sequence_file}"
+        triads = compute_blast_aux(sequences1, sequences2)
         all_triads.extend(triads)
         if write_result:
-            with open(f'data/blast/{species1}/result_{secuence_file.split(".")[0]}_{species2}.csv', 'w') as f:
+            with open(f'data/blast/{species1}/result_{sequence_file.split(".")[0]}_{species2}.csv', 'w') as f:
                 writer = csv.DictWriter(f, fieldnames=['protein_1', 'protein_2', 'bit_score'])
                 writer.writerows(triads)
     if write_result:
@@ -184,80 +180,83 @@ def compute_blast_splitted(species1, species2, write_result=True):
             writer.writerows(all_triads)
     return all_triads
 
+
 def get_most_similar_sequence(result):
     protein1 = result['query_title']
-    if result['hits']:
-        if result['hits'][0]['description']:
-            protein2 = result['hits'][0]['description'][0]['title']
-        else:
-            protein2 = None
-        if result['hits'][0]['hsps']:
-            bitscore = result['hits'][0]['hsps'][0]['bit_score']
-        else:
-            bitscore = 0
-    else:
-        protein2 = None
-        bitscore = 0
-    return {'protein_1': protein1, 'protein_2': protein2, 'bit_score': bitscore}
+    hits = result.get('hits', [{}])[0]
+    protein2 = hits.get('description', [{}])[0].get('title')
+    bit_score = hits.get('hsps', [{}])[0].get('bit_score', 0)
+    return {'protein_1': protein1, 'protein_2': protein2, 'bit_score': bit_score}
 
 
+def train_model(species1, species2, write_result=True):
+    model_path = f'data/models/model_{species1}_{species2}.joblib'
+    scaler_path = f'data/models/scaler_{species1}_{species2}.joblib'
+    if os.path.isfile(model_path) and os.path.isfile(scaler_path):
+        return load(model_path), load(scaler_path)
 
-def train_model(species1, species2):
     training_data, result = build_dataset(species1, species2)
     scaler = StandardScaler()
     scaler.fit(training_data)
-    X_train = scaler.transform(training_data)
-    clf = MLPClassifier(solver='adam', alpha=1e-6, max_iter=30000,hidden_layer_sizes=(5, 2), random_state=1, tol=1e-8)
-    print('start fit')
-    clf.fit(X_train, result).score(X_train, result)
-    print('model trained')
+    train_set = scaler.transform(training_data)
+    clf = MLPClassifier(solver='adam', alpha=1e-6, max_iter=30000, hidden_layer_sizes=(5, 2), random_state=1, tol=1e-8)
+    clf.fit(train_set, result).score(train_set, result)
+    if write_result:
+        dump(clf, model_path)
+        dump(scaler, scaler_path)
     return clf, scaler
 
 
 def set_bar_text(bar_text, text):
-    bar_text.configure(text=text)
-    bar_text.text = text
+    if bar_text:
+        bar_text.configure(text=text)
+        bar_text.text = text
+    else:
+        logger.info(text)
 
-def get_size(info_file, attribute_filter):
-    with open(info_file) as f:
-        reader = csv.DictReader(f)
-        info = list(reader)
-    sizes = {r['ID'] : r[attribute_filter] for r in info}
-    attributes = list(set(sizes.values()))
-    return attributes, sizes
 
-def compute_network(main_species_id, aux_species_id_1, aux_species_id_2, secuences_file, info_file, attribute_filter, bar=None, bar_text=None):
+def set_bar_step(bar, step, old_step):
+    new_step = step + old_step
+    if bar:
+        bar.step(step)
+    else:
+        logger.info(new_step)
+    return new_step
+
+
+def compute_network(main_species_id, aux_species_id_1, aux_species_id_2, sequences_file, bar=None, bar_text=None):
+    prepare_paths()
     if os.path.isfile(f'data/nets/Net_{main_species_id}_{aux_species_id_1}.csv'):
         return None, f'data/nets/Net_{main_species_id}_{aux_species_id_1}.csv'
-    bar.step(0.1)
-    set_bar_text(bar_text, f"Getting secuences for {aux_species_id_1}")
-    get_secuences(aux_species_id_1)
-    bar.step(5)
-    set_bar_text(bar_text, f"Getting secuences for {aux_species_id_2}")
-    get_secuences(aux_species_id_2)
-    bar.step(5)
-
+    bar_step = 0
+    bar_step = set_bar_step(bar, 0.1, bar_step)
+    set_bar_text(bar_text, f"Getting sequences for {aux_species_id_1}")
+    get_sequences(aux_species_id_1)
+    bar_step = set_bar_step(bar, 5, bar_step)
+    set_bar_text(bar_text, f"Getting sequences for {aux_species_id_2}")
+    get_sequences(aux_species_id_2)
+    bar_step = set_bar_step(bar, 5, bar_step)
 
     set_bar_text(bar_text, f"Training model")
     clf, scaler = train_model(aux_species_id_2, aux_species_id_1)
-    bar.step(25)
+    bar_step = set_bar_step(bar, 25, bar_step)
 
-    create_secuences_file(secuences_file, main_species_id)
+    create_sequences_file(sequences_file, main_species_id)
     set_bar_text(bar_text, f"Computing blast between {main_species_id} and {aux_species_id_1}")
     blast_matrix = compute_blast(main_species_id, aux_species_id_1)
-    bar.step(25)
+    bar_step = set_bar_step(bar, 25, bar_step)
     set_bar_text(bar_text, f"Getting {aux_species_id_1} edges")
     edges_output = get_edges(aux_species_id_1)
     prot_bitscore = {r['protein_1']: r['bit_score'] for r in blast_matrix}
     prot_prot = {r['protein_1']: r['protein_2'] for r in blast_matrix}
     prots = list(prot_prot.keys())
-    prots_idx = {p:i for i,p in enumerate(prots)}
+    prots_idx = {prot: idx for idx, prot in enumerate(prots)}
     new_edges = []
     set_bar_text(bar_text, f"Predicting edges")
-    for i in range(len(prots)):
+    total_prots = len(prots)
+    for i in range(total_prots):
         p11 = prots[i]
-        print(f"{i} / {len(prots)}")
-        bar.step(40/len(prots))
+        bar_step = set_bar_step(bar, 40/total_prots, bar_step)
         for j in range(i, len(prots)):
             p12 = prots[j]
             p21 = prot_prot[p11]
@@ -274,9 +273,9 @@ def compute_network(main_species_id, aux_species_id_1, aux_species_id_2, secuenc
                     }
                 )
     with open(f'data/nets/Net_{main_species_id}_{aux_species_id_1}.csv', 'w') as f:
-            writer = csv.DictWriter(f, fieldnames=['protein_1', 'protein_1_idx', 'protein_2', 'protein_2_idx', 'confidence'])
-            writer.writeheader()
-            writer.writerows(new_edges)
-
+        fieldnames = ['protein_1', 'protein_1_idx', 'protein_2', 'protein_2_idx', 'confidence']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(new_edges)
 
     return new_edges, f'data/nets/Net_{main_species_id}_{aux_species_id_1}.csv'
